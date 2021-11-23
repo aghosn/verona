3dd8f098a3cb4348e9c1efeb7f68b4cce90456fe
Adrien Ghosn <ghosn.adrien@gmail.com>
2021-11-23 16:20:05 +0100

    Starting to work on the IR
    Have the initial set of ASTs, now I'll try to parse simple constructs.

--- src/interpreter/ir.h
+++ src/interpreter/ir.h
@@ -7,7 +7,45 @@
 
 namespace verona::ir {
   enum class Kind {
+    
+   Invalid,
+   Identifier,
+   Var,
+   Dup,
+   Load,
+   Store,
+   Lookup,
+   Typetest,
+   NewAlloc,
+   StackAlloc,
+   Call,
+   Tailcall,
+   Region,
+   Create,
+   Branch,
+   Return,
+   Acquire,
+   Release,
+   Fulfill,
+   Function,
+  
+   // Types
+   Iso,
+   Mut,
+   Imm,
+   Paused,
+   Stack,
+
+   // Constant
+   True,
+   False,
+   Undef,
+
+   End,
+
   };
+
+  const char* kindname(Kind k);
 
   struct NodeDef;
 
@@ -54,65 +92,129 @@ namespace verona::ir {
    */
     // Forward definition
   struct Value : Expr {} ;
-  struct Type;
+  struct TypeId;
 
   /**
    * Identifier
    *
    * @comment: I put it in a struct for the moment in case I need more than just
-   * a name.
+   * a name. Maybe replace with a constant or make it extend unescaped string?
    */
   struct Identifier : Expr {
     std::string name;
+
+    Kind kind() override {
+      return Kind::Identifier;
+    }
   };
+
+  // Member ::= Function | Id
+  struct Member : Expr {};
+
+  //TODO(aghosn) quick way to change if I fucked up.
+  //Let's take a litteral approach to the problem.
+  struct ID: Identifier, Member{};
   
-  // x = y
+  // x = Expr
   struct Assign: Expr {
-    Node<Expr> left;
-    Node<Expr> right;
+    Node<ID> left;
+    
+    Kind kind() override {
+      return Kind::Assign;
+    }
+  };
+
+  // x = var
+  struct Var: Assign {
+    Kind kind() override {
+      return Kind::Var;
+    }
+  };
+
+  // x = dup y
+  struct Dup: Assign {
+    Node<ID> y;
+
+    Kind kind() override {
+      return Kind::Dup;
+    }
   };
 
  
-  // store y z
-  struct Load: Expr {
-    Node<Expr> y;
+  // x = load y
+  struct Load: Assign {
+    Node<ID> y;
+
+    Kind kind() override {
+      return Kind::Load;
+    }
   };
 
-  struct Store: Expr {
-    Node<Expr> source;
-    Node<Expr> dest;
+  struct Store: Assign {
+    Node<ID> source;
+    Node<ID> dest;
+
+    Kind kind() override {
+      return Kind::Store;
+    }
   };
 
   // lookup y z
-  struct Lookup: Expr {
-    Node<Expr> y;
-    Node<Expr> z;
+  struct Lookup: Assign {
+    Node<ID> y;
+    Node<ID> z;
+
+    Kind kind() override {
+      return Kind::Lookup;
+    }
   };
 
   // typetest x τ
-  struct Typetest: Expr {
-    Node<Expr> x;
-    Node<Type> type;
+  struct Typetest: Assign {
+    Node<ID> x;
+    Node<TypeId> type;
+
+    Kind kind() override {
+      return Kind::Typetest;
+    }
   };
 
   // new Type
-  struct NewAlloc: Expr {
-    Node<Type> type;
+  struct NewAlloc: Assign {
+    Node<TypeId> type;
+    
+    Kind kind() override {
+      return Kind::NewAlloc;
+    }
   };
 
   // stack Type
-  struct StackAlloc: Expr {
-    Node<Type> type;
+  struct StackAlloc: Assign {
+    Node<TypeId> type;
+
+    Kind kind() override {
+      return Kind::StackAlloc;
+    }
   };
 
-  // call x(y*)
-  struct Call: Expr {
-    Node<Identifier> function;
-    List<Expr> args;
+  // x(y*)
+  struct Apply : Expr {
+    Node<ID> function;
+    List<ID> args;
+  };
+
+  // x = call x(y*)
+  struct Call: Assign, Apply {
+    Kind kind() override {
+      return Kind::Call;
+    }
   };
 
   // tailcall x(x*)
-  struct Tailcall : Call {
+  struct Tailcall : Apply {
+    Kind kind() override {
+      return Kind::Tailcall;
+    }
   };
 
   /**
@@ -126,6 +228,10 @@ namespace verona::ir {
   // region y(z, z*) TODO(aghosn) not sure I understand this.
   struct Region: Scope {
     Node<Identifier> name;
+
+    Kind kind() override {
+      return Kind::Region;
+    }
   };
 
   enum class AllocStrategy {
@@ -136,7 +242,11 @@ namespace verona::ir {
 
   // create Epsi y(z*)
   struct Create: Scope {
-    AllocStrategy strategy; 
+    AllocStrategy strategy;
+
+    Kind kind() override {
+      return Kind::Create;
+    }
   };
 
   
@@ -145,36 +255,59 @@ namespace verona::ir {
     // TODO(aghosn) is that correct?
     Node<Expr> branch1;
     Node<Expr> branch2;
+
+    Kind kind() override {
+      return Kind::Branch;
+    }
   };
 
+  // return x*
   struct Return: Expr {
-    Node<Expr> value;
+    List<ID> returns;
+
+    Kind kind() override {
+      return Kind::Return;
+    }
+  };
+
+  // error
+  struct Error: Expr {
+    Kind kind() override {
+      return Kind::Error; 
+    }
+  };
+
+  // x = catch
+  struct Catch: Assign {
+    Kind kind() override {
+      return Kind::Catch;
+    }
   };
 
   struct Acquire: Expr {
-    Node<Expr> target;
+    Node<ID> target;
+
+    Kind kind() override {
+      return Kind::Acquire;
+    }
   };
 
+  // release x | release v
   struct Release: Expr  {
     Node<Expr> target;
+
+    Kind kind() override {
+      return Kind::Release;
+    }
   };
 
+  // fulfill x
   struct Fulfill: Expr {
-    Node<Expr> target;
-  };
+    Node<ID> target;
 
-  struct Member : Expr {
-    //TODO(aghosn) How to get reference to parent?
-  };
-  
-
-  struct Function : Member, Value {
-    Node<Identifier> name;
-    List<Type> typeparams;
-    // TODO(aghosn) should it be identifiers?
-    List<Expr> args;
-    Node<Type> result; 
-    Node<Expr> body; 
+    Kind kind() override {
+      return Kind::Fulfill;
+    }
   };
 
   /**
@@ -184,16 +317,70 @@ namespace verona::ir {
   struct Constant: Expr {};
 
   /**
-   * Type
+   * Types
    */
-  struct Type: NodeDef
-  {};
+  struct TypeId : Identifier {};
+  struct Type: NodeDef {
+    List<TypeId> typeids;
+    Map<ID, Member> members;
+  };
 
-  struct Iso : Type {};
-  struct Mut : Type {};
-  struct Imm: Type {};
-  struct Paused: Type {};
-  struct Stack: Type {};
+  struct Iso : Type {
+    Kind kind() override {
+      return Kind::Iso;
+    }
+  };
+  struct Mut : Type {
+    Kind kind() override {
+      return Kind::Mut;
+    }
+  };
+  struct Imm: Type {
+    Kind kind() override {
+      return Kind::Imm;
+    }
+  };
+  struct Paused: Type {
+    Kind kind() override {
+      return Kind::Paused;
+    }
+  };
+  struct Stack: Type {
+    Kind kind() override {
+      return Kind::Stack;
+    }
+  };
+
+  struct TypeOp : Type {
+    List<Type> types;
+  };
+
+  struct UnionType: TypeOp {
+    Kind kind() override {
+      return Kind::UnionType;
+    }
+  };
+  struct IsectType: TypeOp {
+    Kind kind() override {
+      return Kind::IsectType;
+    }
+  };
+  struct TupleType: TypeOp {
+    Kind kind() override {
+      return Kind::TupleType;
+    }
+  };
+
+  //TODO  what's this? | {f: T}-> structural interface, f has member of type T
+  struct Interface : Type {
+    List<Member> members;
+  };
+
+  // store T
+  struct StoreType: Type {};
+
+  // ClassID
+  struct ClassID: TypeId {};  
 
   /**
    * Globals for the execution of the program.
@@ -205,18 +392,42 @@ namespace verona::ir {
   //ω       ∈ Object      ::= Region* × TypeId
   struct Object : Value {
     List<Region> regions;
-    Node<Type> typeId;
+    Node<TypeId> typeId;
   };
 
-  struct ObjectId : Value, Identifier {
-    
-  };
+  struct ObjectId : Value, Identifier {};
 
   // Should be used for values direclty?
   // Sounds to me like offset to segment
   // i.e., raw data
   struct StorageLoc : Value, Identifier {
     Node<ObjectId> objectid;
+  };
+
+  struct Function: Value, Identifier, Member {
+    List<ID> ids;
+    List<Expr> exprs;
+
+    Kind kind() override {
+      return Kind::Function;
+    }
+  };
+
+  struct Bool : Value, Constant {};
+  struct True : Bool {
+    Kind kind() override {
+      return Kind::True;
+    }
+  };
+  struct False: Bool {
+    Kind kind() override {
+      return Kind::False;
+    }
+  };
+  struct Undef : Constant {
+    Kind kind() override {
+      return Kind::Undef;
+    }
   };
 
   //TODO(aghosn) instead of havin an id, make it extend a named type.
